8d9d271a3882b76de0b33ca5a90d4d744e9dfeda
Jhon <jhon.charryserrato@gmail.com>
2022-01-14 12:11:38 -0500

    Added new routes

--- app.js
+++ app.js
@@ -24,6 +24,29 @@ app.get("/api/get-uuid", function (req, res) {
   res.send(uuidv4());
 });
 
+// SIMULAR ERROR, CAIDA DEL SERVIDOR
+app.get("/tmp", function (req, res) {
+  const options = {
+    host: "google.com",
+    path: "/",
+  };
+
+  const request = http.request(options, function (r) {
+    let data = "";
+    r.on("data", function (chunk) {
+      data += chunk;
+    });
+    r.on("end", function (chunk) {
+      throw new Error();
+    });
+  });
+
+  request.on("error", function (e) {
+    res.send("err");
+  });
+  request.end();
+});
+
 // 404
 app.get("*", function (req, res) {
   res.status(404).send("Error 404 - Recurso no encontrado");
