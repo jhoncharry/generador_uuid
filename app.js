8e98804ed29e83249e56f0e6c34b0ef0ebedbc64
Jhon <jhon.charryserrato@gmail.com>
2022-01-14 14:11:11 -0500

    Add certificate SSL/TLS

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
+
 // Módulos requeridos
 const express = require("express");
+const cors = require("cors");
+
 const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
 const helmet = require("helmet");
 var compression = require("compression");
-require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
-const cors = require("cors");
+
+const httpsServerOptions = {
+  key: fs.readFileSync(process.env.KEY_PATH),
+  cert: fs.readFileSync(process.env.CERT_PATH),
+};
 
 const app = express();
 app.use(helmet()); // Ayuda a proteger aplicaciones Express
@@ -15,6 +25,16 @@ app.use(cors());
 // Servidor HTTP
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+// Servidor HTTPS
+const serverHttps = https.createServer(httpsServerOptions, app);
+serverHttps.listen(process.env.HTTPS_PORT, process.env.IP);
+
+// Redirect to HTTPS
+app.use((req, res, next) => {
+  if (req.secure) next();
+  else res.redirect(`https://${req.headers.host}${req.url}`);
+});
 
 // Contenido estático
 app.use(express.static("./public"));
